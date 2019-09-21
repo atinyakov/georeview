07dfe960b2da54a0dbfa729e9b233eade4828c79
atinyakov <akoff16@gmail.com>
2019-09-21 15:39:36 +0300

    working baloons

--- src/js/index.js
+++ src/js/index.js
@@ -1,103 +1,85 @@
-let myMap;
-
 ymaps.ready(init);
-function init(){ 
-    // Создание карты.    
-    var myPlacemark;
-    myMap = new ymaps.Map("map", {
-        // Координаты центра карты.
-        // Порядок по умолчанию: «широта, долгота».
-        // Чтобы не определять координаты центра карты вручную,
-        // воспользуйтесь инструментом Определение координат.
-        center: [55.76, 37.64],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 7
-    },{
-        searchControlProvider: 'yandex#search'
-    });
-
-    objectManager = new ymaps.ObjectManager({
-        // Чтобы метки начали кластеризоваться, выставляем опцию.
-        clusterize: true
-        // geoObjectOpenBalloonOnClick: false,
-        // clusterOpenBalloonOnClick: false
-    });
+function init () {
+   var map = new ymaps.Map('map', {
+           center: [55.650625, 37.62708],
+           zoom: 10
+       }, {
+           searchControlProvider: 'yandex#search'
+       })
+    //    counter = 0,
+       // Создание макета содержимого балуна.
+       // Макет создается с помощью фабрики макетов с помощью текстового шаблона.
 
 
-    myMap.geoObjects.add(objectManager);
+       function createBaloon (coords) {
 
-       // Слушаем клик на карте.
-       myMap.events.add('click', function (e) {
+        BalloonContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<form style="margin: 10px;">' +
+                '<b>{{properties.name}}</b><br />' +
+                '<i id="count"></i> ' +
+                '<button id="counter-button"> добавить </button>' +
+            '</form>', {
+            // Переопределяем функцию build, чтобы при создании макета начинать
+            // слушать событие click на кнопке-счетчике.
+            build: function () {
+                // Сначала вызываем метод build родительского класса.
+                BalloonContentLayout.superclass.build.call(this);
+                // А затем выполняем дополнительные действия.
+ 
+                document.getElementById('counter-button').addEventListener('click', (e) => {
+                    e.preventDefault();
+                    this.onCounterClick();
+                 })
+            },
+            // Аналогично переопределяем функцию clear, чтобы снять
+            // прослушивание клика при удалении макета с карты.
+            clear: function () {
+                // Выполняем действия в обратном порядке - сначала снимаем слушателя,
+                // а потом вызываем метод clear родительского класса.
+                BalloonContentLayout.superclass.clear.call(this);
+            },
+            onCounterClick: function () {
+                console.log('here')
+            }
+        });
+
+        var balloon = new ymaps.Balloon(map, {
+            contentLayout: BalloonContentLayout
+        });
+           
+        //    if (balloon.isOpen()) {
+        //        console.log(coords);
+        //        balloon.open(coords);
+        //    }
+        //    else {
+        //        balloon.close();
+        //    }
+
+        balloon.options.setParent(map.options);
+        balloon.open(coords);
+
+        // console.log(balloon.isOpen());
+
+        // if (!balloon.isOpen()) {
+        //     //    console.log(coords);
+        //     balloon.open(coords);
+        // }   
+        //            else {
+        //        balloon.close();
+        //    }
+
+       }
+
+       map.events.add('click', function (e) {
         var coords = e.get('coords');
+        // console.log('before', map.balloon.isOpen());
 
-        // Если метка уже создана – просто передвигаем ее.
-        // if (myPlacemark) {
-        //     myPlacemark.geometry.setCoordinates(coords);
+        // if (!map.balloon.isOpen()) {
+            //    console.log(coords);
+            createBaloon(coords);
+        // }   else {
+        //     map.balloon.close();
         // }
-        // Если нет – создаем.
-        // else {
-            myPlacemark = createPlacemark(coords);
-            myMap.geoObjects.add(myPlacemark);
-            myPlacemark.balloon.open();
-            // Слушаем событие окончания перетаскивания на метке.
-            myPlacemark.events.add('dragend', function () {
-                getAddress(myPlacemark.geometry.getCoordinates());
-            });
-        // }
-        getAddress(coords);
-    });
-
-    // Создание метки.
-    function createPlacemark(coords) {
-        return new ymaps.Placemark(coords, {
-            iconCaption: 'поиск...'
-        }, {
-            preset: 'islands#violetDotIconWithCaption',
-            draggable: false
-        });
-    }
-
-    // Определяем адрес по координатам (обратное геокодирование).
-    function getAddress(coords) {
-        // myPlacemark.properties.set('iconCaption', 'поиск...');
-        ymaps.geocode(coords).then(function (res) {
-            var firstGeoObject = res.geoObjects.get(0);
-
-            myPlacemark.properties
-                .set({
-                    // Формируем строку с данными об объекте.
-                    iconCaption: [
-                        // Название населенного пункта или вышестоящее административно-территориальное образование.
-                        firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
-                        // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
-                        firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
-                    ].filter(Boolean).join(', '),
-                    // В качестве контента балуна задаем строку с адресом объекта.
-                    balloonContent: firstGeoObject.getAddressLine(),
-                    balloonContentBody: 
-                    '<form>' +
-                        '<input type="name" placeholder="имя"></input>' +
-                        '<input type="place" placeholder="место"></input>' +
-                        '<textarea placeholder="Поделитесь впечатлениями"></textarea>' +
-                        '<button type="submit">Добавить</button>' + 
-                    '</form>'
-
-                });
-        });
-    }
+        
+       });
 }
-
-// var placemark = new ymaps.Placemark(myMap.getCenter(), {
-//     // Зададим содержимое заголовка балуна.
-//     balloonContentHeader: '<a href = "#">Рога и копыта</a><br>' +
-//         '<span class="description">Сеть кинотеатров</span>',
-//     // Зададим содержимое основной части балуна.
-//     balloonContentBody: '<img src="img/cinema.jpg" height="150" width="200"> <br/> ' +
-//         '<a href="tel:+7-123-456-78-90">+7 (123) 456-78-90</a><br/>' +
-//         '<b>Ближайшие сеансы</b> <br/> Сеансов нет.',
-//     // Зададим содержимое нижней части балуна.
-//     balloonContentFooter: 'Информация предоставлена:<br/>OOO "Рога и копыта"',
-//     // Зададим содержимое всплывающей подсказки.
-//     hintContent: 'Рога и копыта'
-// });
