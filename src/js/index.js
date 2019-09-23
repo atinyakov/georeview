e3ad58fa742d3249bb081db265ca2be721867550
atinyakov <akoff16@gmail.com>
2019-09-23 23:53:01 +0300

    working baloons

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,31 @@
 ymaps.ready(init);
 function init () {
-   var map = new ymaps.Map('map', {
-           center: [55.650625, 37.62708],
-           zoom: 10
-       }, {
-           searchControlProvider: 'yandex#search'
-       })
-    //    counter = 0,
+    var map = new ymaps.Map('map', {
+            center: [55.650625, 37.62708],
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        objectManager = new ymaps.ObjectManager({
+            // Чтобы метки начали кластеризоваться, выставляем опцию.
+            clusterize: true,
+            // ObjectManager принимает те же опции, что и кластеризатор.
+            gridSize: 32,
+            clusterDisableClickZoom: true
+        });
+
        // Создание макета содержимого балуна.
        // Макет создается с помощью фабрики макетов с помощью текстового шаблона.
 
 
-       function createBaloon (coords) {
-
+        function createBaloon (e) {
+        var coords = e.get('coords');
+        
         BalloonContentLayout = ymaps.templateLayoutFactory.createClass(
             '<form style="margin: 10px;">' +
                 '<b>{{properties.name}}</b><br />' +
                 '<i id="count"></i> ' +
-                '<button id="counter-button"> добавить </button>' +
+                '<a id="counter-button"> добавить </a>' +
             '</form>', {
             // Переопределяем функцию build, чтобы при создании макета начинать
             // слушать событие click на кнопке-счетчике.
@@ -27,9 +35,8 @@ function init () {
                 // А затем выполняем дополнительные действия.
  
                 document.getElementById('counter-button').addEventListener('click', (e) => {
-                    e.preventDefault();
-                    this.onCounterClick();
-                 })
+                    this.onCounterClick(e);
+                })
             },
             // Аналогично переопределяем функцию clear, чтобы снять
             // прослушивание клика при удалении макета с карты.
@@ -38,48 +45,34 @@ function init () {
                 // а потом вызываем метод clear родительского класса.
                 BalloonContentLayout.superclass.clear.call(this);
             },
-            onCounterClick: function () {
-                console.log('here')
+            onCounterClick: function (e) {
+                e.preventDefault();
+                var placemark = new ymaps.Placemark(coords, {
+                    name: 'Считаем'
+                }, {
+                    balloonContentLayout: BalloonContentLayout,
+                    // Запретим замену обычного балуна на балун-панель.
+                    // Если не указывать эту опцию, на картах маленького размера откроется балун-панель.
+                    balloonPanelMaxMapArea: 0
+                });
+        
+                map.geoObjects.add(placemark);
+                balloon.close();
             }
         });
 
         var balloon = new ymaps.Balloon(map, {
             contentLayout: BalloonContentLayout
         });
-           
-        //    if (balloon.isOpen()) {
-        //        console.log(coords);
-        //        balloon.open(coords);
-        //    }
-        //    else {
-        //        balloon.close();
-        //    }
 
         balloon.options.setParent(map.options);
         balloon.open(coords);
 
-        // console.log(balloon.isOpen());
-
-        // if (!balloon.isOpen()) {
-        //     //    console.log(coords);
-        //     balloon.open(coords);
-        // }   
-        //            else {
-        //        balloon.close();
-        //    }
-
        }
 
-       map.events.add('click', function (e) {
-        var coords = e.get('coords');
-        // console.log('before', map.balloon.isOpen());
+        map.events.add('click', function (e) {
 
-        // if (!map.balloon.isOpen()) {
-            //    console.log(coords);
-            createBaloon(coords);
-        // }   else {
-        //     map.balloon.close();
-        // }
-        
-       });
+        console.log(map.balloon.isOpen());     
+            createBaloon(e);      
+        });
 }
