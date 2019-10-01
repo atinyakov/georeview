b80fbcc9494b4d17a17960c67f3407513ac80efd
atinyakov <akoff16@gmail.com>
2019-10-02 01:22:16 +0300

    done

--- src/js/index.js
+++ src/js/index.js
@@ -23,12 +23,14 @@ function init () {
     window.clusterer.events.add('click', (e) => {
         var object = e.get('target');
 
+        if (window.balloon) {
+            window.balloon.close();
+        }
 
         if (!object.getGeoObjects) {
             // клик был по метке   
             createBaloon(object.geometry._coordinates, object);
-        }
-
+        } 
     })
 
     document.addEventListener('click', (e) => {
@@ -44,116 +46,43 @@ function init () {
     })
 
 
-    if(localStorage.data !== undefined) {
+    if (localStorage.data !== undefined) {
         var pins = JSON.parse(localStorage.data);
+
         pins.forEach((pin) => {
+            mapEls[idx] = { coords: pin.coords, name: pin.name, point: pin.point, date: pin.date, message: pin.message}
+            idx++;
+
+            ymaps.geocode(pin.coords)
+            .then(function (res) {
+            var address = res.geoObjects.get(0).properties.get('text');
+            var d = new Date(Date.parse(pin.date));
 
             var placemark = new ymaps.Placemark(pin.coords, {
                 balloonContentHeader: `<b>${pin.point}</b>`,
-                balloonContentBody: `<div id="review"><a class="linkCoords" href="javascript:void(0);" data-coords="${pin.coords}"></a> <p>${pin.message}</p></div>`
-                // balloonContentFooter: `${d.getDate()}.${d.getMonth()}.${d.getFullYear()} ${d.getHours()}.${d.getMinutes()}`,
+                balloonContentBody: `<div id="review"><a class="linkCoords" href="javascript:void(0);" data-coords="${pin.coords}">${address}</a> <p>${pin.message}</p></div>`,
+                balloonContentFooter: `${d.getDate()}.${d.getMonth()}.${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`,
             }, {
-                balloonContentLayout: ymaps.templateLayoutFactory.createClass(
-                    '<div class="form">' + 
-                    '<form >' +
-                        '<div class="header">' +'</div>' +
-                        '<div class="body">' + 
-                        '</div>' + 
-                        '<p class="title">Ваш отзыв</p>' +
-                        '<div>' + 
-                        '<input id="name" type="name" placeholder="Имя">' + 
-                        '</div>' + 
-                        '<div>' + 
-                        '<input id="point" type="text" placeholder="Укажите место">' +
-                        '</div>' + 
-                        '<div>' + 
-                            `<textarea id="message" placeholder="Поделись впечатлениями"></textarea>` +
-                        '</div>' + 
-                        '<div class="button">' + 
-                        '<button id="counter-button"> добавить </button>' +
-                        '</div>' + 
-                    '</form>' +
-                    '</div>'
-                    , {
-                    // Переопределяем функцию build, чтобы при создании макета начинать
-                    // слушать событие click на кнопке-счетчике.
-                    build: function () {
-                        // Сначала вызываем метод build родительского класса.
-                        BalloonContentLayout.superclass.build.call(this);
-                        // А затем выполняем дополнительные действия
-        
-                        if (data.length > 0) {
-                            for (const key in data) {
-                                if (data.hasOwnProperty(key)) {
-                                    const body = document.querySelector('.body');
-                                    const div = document.createElement('div');
-                                    div.innerHTML = data[key].message;
-                                    body.appendChild(div);
-                                }
-                            }
-                        }
-        
-                        var form = document.querySelector('.form');  
-                        document.getElementById('counter-button').addEventListener('click', (e) => {
-                            e.preventDefault();
-        
-                            var form = document.querySelector('.form');
-                            var name = form.querySelector('#name').value || pin.name;
-                            var point = form.querySelector('#point').value || pin.point;
-                            var message = form.querySelector('#message').value || pin.message;
-                            var d = new Date();
-            
-                            var body = form.querySelector('.body');
-        
-                            var fragment = document.createElement('div');
-                            fragment.innerHTML = `<div id="review"><b>${name}</b> <span>${point} </span><span class="data">${d.getDate()}.${d.getMonth()}.${d.getFullYear()} ${d.getHours()}.${d.getMinutes()}</span><p>${message}</p></div>`;
-                            body.appendChild(fragment);
-
-                            this.addContent(coords, name, message, point, d);
-                        })
-                    },
-                    // Аналогично переопределяем функцию clear, чтобы снять
-                    // прослушивание клика при удалении макета с карты.
-                    clear: function () {
-                        // Выполняем действия в обратном порядке - сначала снимаем слушателя,
-                        // а потом вызываем метод clear родительского класса.
-                        BalloonContentLayout.superclass.clear.call(this);
-                    },
-                    addContent: function (coords, name, message, point, d) {
-        
-                        var storage = localStorage;
-                        var dataPins = [];
-        
-                        if(storage.data) {
-                            dataPins = JSON.parse(storage.data);
-                        }
-        
-        
-                        mapEls[idx] = { coords: coords, name: name, message: `<div id="review"><b>${name}</b> <span>${point}</span><span class="data">${d.getDate()}.${d.getMonth()}.${d.getFullYear()} ${d.getHours()}.${d.getMinutes()}</span><p>${message}</p></div>`}
-                        dataPins.push(mapEls[idx])
-
-                        storage.data = JSON.stringify(dataPins);
-                        
-                        console.log(storage);
-                        idx++;
-        
-        
-                        var placemark = new ymaps.Placemark(coords, {
-                            balloonContentHeader: `<b>${point}</b>`,
-                            balloonContentBody: `<div id="review"><a class="linkCoords" href="javascript:void(0);" data-coords="${coords}">${points}</a> <p>${message}</p></div>`,
-                            balloonContentFooter: `${d.getDate()}.${d.getMonth()}.${d.getFullYear()} ${d.getHours()}.${d.getMinutes()}`,
-                        }, {
-                            balloonContentLayout: BalloonContentLayout,
-                            // Запретим замену обычного балуна на балун-панель.
-                            // Если не указывать эту опцию, на картах маленького размера откроется балун-панель.
-                            balloonPanelMaxMapArea: 0,
-                            hasBalloon: false
-                        });
-                
-                        window.clusterer.add(placemark);
-                        // window.balloon.close();
-                    }
-                }),
+                balloonContentLayout: '<div class="form">' + 
+                        '<form >' +
+                            '<div class="header">' +'</div>' +
+                            '<div class="body">' + 
+                            '</div>' + 
+                            '<p class="title">Ваш отзыв</p>' + 
+                            '<div>' + 
+                            '<input id="name" type="name" placeholder="Имя">' + 
+                            '</div>' + 
+                            '<div>' + 
+                            '<input id="point" type="text" placeholder="Укажите место">' +
+                            '</div>' + 
+                            '<div>' + 
+                                `<textarea id="message" placeholder="Поделись впечатлениями"></textarea>` +
+                            '</div>' + 
+                            '<div class="button">' + 
+                            '<button id="counter-button"> добавить </button>' +
+                            '</div>' + 
+                        '</form>' +
+                        '</div>',
                 // Запретим замену обычного балуна на балун-панель.
                 // Если не указывать эту опцию, на картах маленького размера откроется балун-панель.
                 balloonPanelMaxMapArea: 0,
@@ -161,6 +90,7 @@ function init () {
             });
     
             window.clusterer.add(placemark);
+            })
         })
     }
 
@@ -170,7 +100,6 @@ function init () {
     // Макет создается с помощью фабрики макетов с помощью текстового шаблона.
 
     function createBaloon (coords, object) {
-
         var data = [];
 
         // добавляем новый пин в data
@@ -198,7 +127,6 @@ function init () {
                 }
             }
         }
-
 
         ymaps.geocode(coords)
             .then(function (res) {
@@ -234,9 +162,6 @@ function init () {
                 BalloonContentLayout.superclass.build.call(this);
                 // А затем выполняем дополнительные действия
 
-
-                // const point = document.getElementById('point').value;
-                // var that = this;
                 if (data.length > 0) {
                     for (const key in data) {
                         if (data.hasOwnProperty(key)) {
@@ -285,14 +210,15 @@ function init () {
                     dataPins = JSON.parse(storage.data);
                 }
 
-                mapEls[idx] = { coords: coords, name: name, point: point, message: `<div id="review"><b>${name}</b> <span>${point}</span><span class="data">${d.getDate()}.${d.getMonth()}.${d.getFullYear()} ${d.getHours()}.${d.getMinutes()}</span><p>${message}</p></div>`}
+                if(localStorage.data !== undefined) {
+                    idx = localStorage.data.length + 1;
+                }
+
+                mapEls[idx] = { coords: coords, name: name, point: point, date: d, message: `<div id="review"><b>${name}</b> <span>${point}</span><span class="data">${d.getDate()}.${d.getMonth()}.${d.getFullYear()} ${d.getHours()}.${d.getMinutes()}</span><p>${message}</p></div>`}
                 dataPins.push(mapEls[idx])
 
                 storage.data = JSON.stringify(dataPins);
-                
-                console.log(storage);
                 idx++;
-
 
                 var placemark = new ymaps.Placemark(coords, {
                     balloonContentHeader: `<b>${point}</b>`,
@@ -322,11 +248,9 @@ function init () {
     }
 
     map.events.add('click', function (e) {
-
         if (window.balloon) {
             window.balloon.close();
         }
-
         var coords = e.get('coords');
 
         createBaloon(coords);      
